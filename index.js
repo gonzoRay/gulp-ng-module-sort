ceb0b44d6fabfe3f33cdaa8dd0ba92fc8f249b3c
Reagan Cooper <rcooper@morneaushepell.com>
2015-03-23 10:49:08 -0400

    working version 1

---
+++ index.js
@@ -0,0 +1,41 @@
+var __path = require('path');
+var __through = require('through2');
+
+module.exports = function(options) {
+    options = options || {};
+
+    if(!!options) {
+        options.fileType = '.js';
+    }
+
+    var moduleFiles = [];
+    var otherFiles = [];
+    var sortedFiles = [];
+
+    return __through.obj(onFile, onEnd);
+
+    function onFile(file, enc, cb) {
+        var absolutePath = file.path
+        var ext = __path.extname(absolutePath);
+        switch (ext) {
+            case options.fileType:
+                var baseFilename = __path.basename(absolutePath, ext).toLowerCase()
+                var parentDirectory = __path.dirname(absolutePath).split('\\').slice(-1)[0];
+                if(baseFilename.toLowerCase() === parentDirectory.toLowerCase()) {
+                    moduleFiles.push(file);
+                } else {
+                    otherFiles.push(file);
+                }
+                break;
+        }
+        cb();
+    }
+    function onEnd(cb) {
+        var that = this;
+        sortedFiles = moduleFiles.concat(otherFiles);
+        sortedFiles.forEach(function(file) {
+            that.push(file);
+        });
+        cb();
+    }
+};
